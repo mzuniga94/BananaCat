8502a8e42b271da3868702f08a2ac783f4af8da4
Zulema Perez <rancid80s@csu.fullerton.edu>
2018-10-17 18:43:47 -0700

    Added Files

---
+++ environment/README.md
@@ -0,0 +1,29 @@
+# Welcome to Banana Cat!
+
+This guide has information about stuff you'll need to know for developing in this project!
+
+## For Front-End & Back-End Developers:
+
+Main Terminal Commands:
+
+Change Directory: cd <DIRECTORY NAME>
+Open File in Tab: c9 open <FILENAME>
+Run the Server: Navigate to the Root folder -> node app.js
+
+Create new file: touch <FILENAME.EXTENSION>
+Create new directory: mkdir <DIRECTORY NAME>
+
+Go up one directory: cd ..
+Reset the terminal: reset
+Clear the terminal: clear
+
+Useful Tips:
+
+Press <TAB> to autocomplete file names or directories in the terminal. For example, 
+you have a directory called ExpressStuff. You can type cd Exp and then TAB to
+autocomplete.
+
+## Best Practices: Front-End
+
+* For every EJS file you create, please comment on the first line the title and directory of the file.
+* Example: <!-- views/pages/index.ejs -->
---
+++ environment/app.js
@@ -0,0 +1,81 @@
+// Says that the project will need the Express library
+var express = require("express");
+// Create the express() object.
+var app = express();
+
+// TODO: Put MySQL dependencies here. 
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+   host: 'bananacat.c7dnoyrsskf4.us-west-1.rds.amazonaws.com',
+   port: '3306',
+   user: 'BananaCat362',
+   password: 'catcpsc362',
+   database: 'JonathanBedoy'
+});
+
+var results = null;
+var n = "Bedoy";
+var templateA = "SELECT * FROM User WHERE LastName = ";
+
+connection.connect(function(err) {
+   if(err) {
+      console.error('Database connection failed: ' + err.stack);
+      return;
+   }
+   console.log('Connected to database');
+   if (err) throw err;
+   
+   connection.query("SELECT * FROM User WHERE LastName = 'Bedoy'", function (err, result, fields) {
+    if (err) throw err;
+    var results = result;
+    console.log(result);
+    app.get('/myaccount', function(req, res){
+   res.render('pages/myaccount',{user: result});
+});
+
+  });
+});
+
+// Set the view engine to EJS. This means we're loading dynamic HTML files through EJS.
+app.set('view engine', 'ejs');
+
+// Renders the home/index page.
+app.get('/', function(req, res){ 
+   res.render('pages/index');
+});
+
+app.get('/products', function(req, res){
+   res.render('pages/products');
+});
+
+app.get('/apparel', function(req, res){
+   res.render('pages/apparel'); 
+});
+
+app.get('/register', function(req, res){
+   res.render('pages/register');
+});
+
+app.get('/login', function(req, res){
+   res.render('pages/login');
+});
+
+
+
+app.get('/booksandmanga', function(req, res){
+   res.render('pages/booksandmanga');
+});
+
+app.get('/figurines', function(req, res){
+   res.render('pages/figurines');
+});
+
+app.get('/snacks', function(req, res){
+   res.render('pages/snacks');
+});
+
+// Starts the server!
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("Server has started!");
+});
